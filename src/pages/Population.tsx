63d89ca728ce024711f88b178ce4ba6e67acd6ce
kk-zu <kaxxxxzu@gmail.com>
2021-02-06 23:11:30 +0900

    グラフを表示する

--- src/pages/Population.tsx
+++ src/pages/Population.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { fetchPopulation } from 'src/apis/resas_api/populations'
 import { fetchPrefectures } from 'src/apis/resas_api/prefectures'
 import LineGraph from 'src/components/commmon/LineGraph'
@@ -10,75 +10,83 @@ import { Prefecture } from 'src/types/prefectures/prefectures'
 import './scss/Population.scss'
 
 type PopulatiomGraphItem = LineGraphItem & { prefectureId: string }
-type State = {
-  prefectures: Array<SelectorItem>
-  populations: Array<PopulatiomGraphItem>
-  selectedPrefectureIds: Array<string>
-}
 
-export default class Population extends React.Component<{}, State> {
-  constructor(props: any) {
-    super(props)
-    this.state = { prefectures: [], populations: [], selectedPrefectureIds: [] }
-  }
+const Population: React.FC = () => {
+  const [prefectures, setPrefectures] = useState<Array<SelectorItem>>([])
+  const [populations, setPopulations] = useState<Array<PopulatiomGraphItem>>([])
+  const [selectedPrefectureIds, setSelectedPrefectureIds] = useState<
+    Array<string>
+  >([])
 
-  async componentDidMount() {
-    await fetchPrefectures()
+  useEffect(() => {
+    fetchPrefectures()
       .then((prefectures: Array<Prefecture>) => {
-        this.setState({
-          prefectures: prefectures.map((prefecture) => {
+        setPrefectures(
+          prefectures.map((prefecture) => {
             return {
               id: prefecture.prefCode.toString(),
               name: prefecture.prefName,
             }
           }),
-        })
+        )
       })
       .catch(() => {
         // エラー画面でリトライを促すメッセージを表示する
         // エラー処理は大抵はアプリケーションごとに決まっているため今回は省く
       })
-  }
+  }, [])
 
-  setStateSelectedPrefectureIds(prefectureId: string): boolean {
-    const currentSelectedPrefectureIds = this.state.selectedPrefectureIds
-    if (currentSelectedPrefectureIds.includes(prefectureId)) {
-      this.setState({
-        selectedPrefectureIds: currentSelectedPrefectureIds.filter(
-          (id) => id !== prefectureId,
-        ),
-      })
+  /**
+   * 選択された都道府県のみグラフに描画するための人口を保持する
+   */
+  const populationsForDraw = useMemo(() => {
+    return populations.filter((population) => {
+      return selectedPrefectureIds.includes(population.prefectureId)
+    })
+  }, [populations, selectedPrefectureIds])
+
+  /**
+   * 選択された都道府県IDを保持する
+   * @returns {boolean} true:保持、false:破棄
+   */
+  const setStateSelectedPrefectureIds = (prefectureId: string): boolean => {
+    if (selectedPrefectureIds.includes(prefectureId)) {
+      setSelectedPrefectureIds(
+        selectedPrefectureIds.filter((id) => id !== prefectureId),
+      )
       return false
     } else {
-      this.setState({
-        selectedPrefectureIds: [...currentSelectedPrefectureIds, prefectureId],
-      })
+      setSelectedPrefectureIds([...selectedPrefectureIds, prefectureId])
       return true
     }
   }
 
-  hasPopulation(prefectureId: string): boolean {
-    return this.state.populations.some(
+  /**
+   * 人口データを保持していたらtrue
+   */
+  const hasPopulation = (prefectureId: string): boolean => {
+    return populations.some(
       (population) => population.prefectureId === prefectureId,
     )
   }
 
-  async fetchPopulationsByPrefectureId(prefectureId: string) {
+  /**
+   * 都道府県の人口を取得する
+   */
+  const fetchPopulationsByPrefectureId = (prefectureId: string) => {
     // 取得済みは再取得しない
     if (
-      this.setStateSelectedPrefectureIds(prefectureId) &&
-      this.hasPopulation(prefectureId)
+      !setStateSelectedPrefectureIds(prefectureId) ||
+      hasPopulation(prefectureId)
     )
       return
-    // TODO: リクエストの回数制限気をつける
-    // TODO: status=429の時は0.5秒待ってリトライ
     const requestParams: PopulationsRequest = {
       prefCode: prefectureId,
       cityCode: '-',
     }
-    await fetchPopulation(requestParams)
+    fetchPopulation(requestParams)
       .then((population) => {
-        const selectedPrefecture = this.state.prefectures.find(
+        const selectedPrefecture = prefectures.find(
           (prefecture) => prefecture.id === prefectureId,
         )
         // selectedPrefectureが存在しない時、画面にも描画されていないので無言で処理をやめる
@@ -89,40 +97,38 @@ export default class Population extends React.Component<{}, State> {
         if (!populationDataForDraw) return // 選択した都道府県に人口のデータがないことを伝える
         const convertedPopulationForGraph: PopulatiomGraphItem = {
           id: selectedPrefecture.name,
-          color: '',
           data: populationDataForDraw.data.map((datum) => {
             return { x: datum.year, y: datum.value }
           }),
           prefectureId: prefectureId,
         }
-        this.setState({
-          populations: [...this.state.populations, convertedPopulationForGraph],
-        })
+        setPopulations([...populations, convertedPopulationForGraph])
       })
       .catch(() => {
         // トースター等でリトライを促すメッセージを表示する
         // エラー処理は大抵はアプリケーションごとに決まっているため今回は省く
       })
   }
+  const title = '都道府県一覧'
+  const description = 'グラフを表示したい都道府県をチェックしてください。'
+  const graphEmptyTitle =
+    '人口推移を表示したい都道府県を選択するとグラフが表示されます。'
 
-  render() {
-    const title = '都道府県一覧'
-    const description =
-      '人口推移のグラフを表示したい都道府県をチェックしてください。'
-    return (
-      <div className="population">
-        <div className="population__prefectures">
-          <SelectorList
-            selectors={this.state.prefectures}
-            title={title}
-            description={description}
-            onCheck={this.fetchPopulationsByPrefectureId}
-          ></SelectorList>
-        </div>
-        <div className="population__graph">
-          <LineGraph lines={this.state.populations} />
-        </div>
+  return (
+    <div className="population">
+      <div className="population__prefectures">
+        <SelectorList
+          selectors={prefectures}
+          title={title}
+          description={description}
+          onCheck={fetchPopulationsByPrefectureId}
+        ></SelectorList>
       </div>
-    )
-  }
+      <div className="population__graph">
+        <LineGraph lines={populationsForDraw} emptyText={graphEmptyTitle} />
+      </div>
+    </div>
+  )
 }
+
+export default Population
--- src/types/common/graph.ts
+++ src/types/common/graph.ts
@@ -1,5 +1,5 @@
 export type LineGraphItem = {
   id: string
-  color: string
+  color?: string
   data: Array<{ x: string | number; y: number }>
 }
